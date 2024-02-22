22b6e9fb632bdc3ae114a70ebbf201722c65fe86
Jose Fenic Peiteado Padilla <alu0101227023@ull.edu.es>
2024-02-22 09:24:52 +0000

    FUNCIONES

--- src/clean_code/function.ts
+++ src/clean_code/function.ts
@@ -88,3 +88,87 @@ function createTempFile(name: string) {
 function createFile(name: string) {
   fs.create(name);
 }
+
+
+
+// Dont use flags as function parameters
+
+/**
+ * Creates a file with a specified name and allows 
+ * the option to mark it  as temporary or not.
+ * @param name The name of the file.
+ * @param temp A boolean flag indicating whether the file is temporary or not.
+ */
+function createFile(name: string, temp: boolean) {
+  if (temp) {
+    fs.create(`./temp/${name}`);
+  } else {
+    fs.create(name);
+  }
+}
+
+/**
+ * Create a file temporary file with the specified name
+ * @param name The name of the temporary file
+ */
+function createTempFile(name: string) {
+  createFile(`./temp/${name}`);
+}
+
+/**
+ * Creates a file with the specified name
+ * @param name The name of the file
+ */
+function createFile(name: string) {
+  fs.create(name);
+}
+
+
+// Encapsulate conditionals
+
+// Checks various situations to determine the course of action. However...
+if (subscription.isTrial || account.balance > 0) {
+  // ...
+}
+
+
+/**
+ * Determines if a service can be activated based on the subscription and account status.
+ * @param subscription The subscription object containing information about the subscription.
+ * @param account The account object containing information about the user's account.
+ * @returns A boolean value indicating whether the service can be activated.
+ */
+function canActivateService(subscription: Subscription, account: Account) {
+  return subscription.isTrial || account.balance > 0;
+}
+
+
+if (canActivateService(subscription, account)) {
+  // ...
+}
+
+
+// Avoid negative conditionals
+
+// Avoid type checking
+
+// TypeScript is a strict syntactical superset of JavaScript and adds optional 
+// static type checking to the language. Always prefer to specify types of variables, 
+// parameters and return values to leverage the full power of TypeScript features. 
+// It makes refactoring more easier.
+
+function travelToTexas(vehicle: Bicycle | Car) {
+  if (vehicle instanceof Bicycle) {
+    vehicle.pedal(currentLocation, new Location('texas'));
+  } else if (vehicle instanceof Car) {
+    vehicle.drive(currentLocation, new Location('texas'));
+  }
+}
+
+type Vehicle = Bicycle | Car;
+
+
+function travelToTexas(vehicle: Vehicle) {
+  vehicle.move(currentLocation, new Location('texas'));
+}
+
